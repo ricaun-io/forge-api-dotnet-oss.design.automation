7ca38b8fd0de955e8ada53a0cb30d4b9853d1bdf
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-08-20 20:26:02 -0300

    Pre release beta

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,7 +3,7 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-class Build : NukeBuild, IPublishPack, ITestLocal
+class Build : NukeBuild, IPublishPack, ITestLocal, IPrePack
 {
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
